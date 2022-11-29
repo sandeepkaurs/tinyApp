5fd89cd7b81ec7139c9859533ee10dd5038435ad
sandeepkaurs <sandeep.singh@telus.com>
2022-11-29 07:16:19 -0800

    Added generateRandomString to express_server

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+function generateRandomString() {}
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -5,6 +7,11 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
