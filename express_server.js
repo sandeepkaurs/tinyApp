91fb2e64e8759346a16b881352d02042bb58b09a
sandeepkaurs <sandeep.singh@telus.com>
2022-11-30 20:49:48 -0800

    Added cookies

--- express_server.js
+++ express_server.js
@@ -7,6 +7,8 @@ const generateRandomString = () => {
 
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser")
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
@@ -40,16 +42,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {username: req.cookies["username"], urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const username = req.cookies.username;
+  const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id], username : username};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id]};
+  const username = req.cookies.username;
+  const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id], username : username};
   res.render("urls_show", templateVars);
 });
 
@@ -78,6 +83,16 @@ app.post("/urls/:id", (req, res) => {
     res.redirect("/urls");
 })
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls');
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
